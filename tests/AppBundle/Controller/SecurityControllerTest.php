062969103b99df2524ba8aec748773920f766e79
samir <allabsamir777@gmail.com>
2019-10-28 21:32:49 +0100

    Security controller Functional tests loginAction,check_login and logout

---
+++ tests/AppBundle/Controller/SecurityControllerTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Tests\AppBundle\Controller;
+use AppBundle\Entity\User;
+use AppBundle\Controller\SecurityController;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class SecurityControllerTest extends WebTestCase
+{
+    protected $controller;
+    public function setUp():void 
+    {
+        $this->controller = new SecurityController();
+    }
+    public function testLoginActionValidCredentials()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/login');
+        $form = $crawler->selectButton('Se connecter')->form();
+        $form['_username'] = 'samir123';
+        $form['_password'] = 'samir';
+        $crawler = $client->submit($form);
+        $this->assertTrue($client->getResponse()->isRedirect());
+        $client->followRedirect();       
+        $this->assertEquals(200,$client->getResponse()->getStatusCode());
+        $this->assertStringContainsString('Consulter la liste des tâches à faire',$client->getResponse()->getContent());
+    }
+    public function testLoginActionInvalideCredentials()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/login');
+        $form = $crawler->selectButton('Se connecter')->form();
+        $form['_username'] = 'user';
+        $form['_password'] = 'admin';
+        $crawler = $client->submit($form);
+        $this->assertTrue($client->getResponse()->isRedirect());
+        $crawler = $client->followRedirect(); 
+        $this->assertSame(1, $crawler->filter('div.alert.alert-danger:contains("Invalid credentials.")')->count());
+        $this->assertEquals(200,$client->getResponse()->getStatusCode());
+    }
+    public function testLoginCheck()
+    {
+        $check = $this->controller->loginCheck();
+        self::assertNull( $check );
+    }
+    public function testLogout()
+    {
+        $check = $this->controller->logoutCheck();
+        self::assertNull( $check );
+    }
+}
