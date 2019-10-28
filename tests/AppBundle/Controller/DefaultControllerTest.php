643441a7f0cc5fa2d482fd3c4237df328509bdaa
samir <allabsamir777@gmail.com>
2019-10-28 21:28:30 +0100

    defaultController functional test requesting root with tow case annonymous/authentified user

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -6,13 +6,20 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
-    public function testIndex()
+    public function testIndexRedirectionToLogin()
     {
         $client = static::createClient();
-
         $crawler = $client->request('GET', '/');
-
+        $crawler = $client->followRedirects();
+        $this->assertEquals(302, $client->getResponse()->getStatusCode());
+        $this->assertStringContainsString('Redirecting to http://localhost/login',$client->getResponse()->getContent());
+    }
+    public function testIndexAfetrLogin()
+    {
+        $client = static::createClient( [], ['PHP_AUTH_USER' => 'samir123', 'PHP_AUTH_PW' => 'samir'] );
+        $client->request( 'GET', '/');
+        $this->assertFalse($client->getResponse()->isRedirect());
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
+        $this->assertStringContainsString('Consulter la liste des tâches à faire',$client->getResponse()->getContent());
     }
 }
